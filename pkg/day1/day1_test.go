9c42dd47a4204e81afd3bc07efcafc65710dc299
Moritz Bracht <mail@morz.me>
2020-12-27 22:59:37 +0100

    Add implementation of Day 1 Part 2
    Almost a full copy paste of Part 1, but DRYing this up does not add any
    value.

--- pkg/day1/day1_test.go
+++ pkg/day1/day1_test.go
@@ -46,3 +46,41 @@ func TestPart1(t *testing.T) {
 		})
 	}
 }
+
+func TestPart2(t *testing.T) {
+	tests := map[string]struct {
+		r         io.Reader
+		expected  int
+		expectErr bool
+	}{
+		"example": {
+			strings.NewReader(`1721
+979
+366
+299
+675
+1456`),
+			241861950,
+			false,
+		},
+		"empty input": {
+			strings.NewReader(``),
+			0,
+			true,
+		},
+		"not a number": {
+			strings.NewReader(`foo`),
+			0,
+			true,
+		},
+	}
+	for name, tt := range tests {
+		t.Run(name, func(t *testing.T) {
+			actual, err := day1.PartTwo(tt.r)
+			if tt.expectErr {
+				assert.Error(t, err)
+			}
+			assert.Equal(t, tt.expected, actual)
+		})
+	}
+}
